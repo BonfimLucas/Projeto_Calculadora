83269bedd73388ffa224198e998bfccc6332ae4a
Lucas <lucasbonfim604@gmail.com>
2023-09-27 20:51:08 -0300

    scriptMudançaEstilo

--- assets/script/natureTheme.js
+++ assets/script/natureTheme.js
@@ -1,1 +1,18 @@
 // https://color.adobe.com/pt/search?q=nature
+
+function natureMode(){
+
+    document.getElementsByTagName("body")[0].style = 'background-image:url("assets/style/imagens/wallpaper.jpg")'
+    document.getElementsByClassName("label")[0].style = 'color: white'
+
+
+    document.getElementsByTagName("img")[0].src = 'assets/style/imagens/github-clean.png'
+    document.getElementsByTagName("img")[1].src = 'assets/style/imagens/linkedin-clean.png'
+    document.getElementsByTagName("img")[2].src = 'assets/style/imagens/instagram-clean.png'
+
+    document.getElementsByTagName("main")[0].style = 'background-color: rgba(0, 6, 13, 0.564)'
+
+    document.getElementsByTagName('input')[0].style = 'background-color: rgba(14, 38, 24, 0.564)'
+
+    document.getElementById('engualkkkk').style = 'background-color: rgba(45, 89, 48, 0.564)'
+}
